04babd021beaeb47a6f6881d3b5175b32625551c
Barbara Kosińska <basia7kosinska@gmail.com>
2023-12-19 22:24:58 +0100

    Add Hero & SearchForm class

---
+++ first-react-pack/src/App.js
@@ -0,0 +1,17 @@
+
+import Hero from './components/Hero/Hero'
+import SearchForm from './components/SearchForm/SearchForm'
+
+const App = () => {
+  return (
+    <div>
+      <Hero />
+      <SearchForm />
+    </div>
+  );
+};
+
+export default App;
+
+
+
