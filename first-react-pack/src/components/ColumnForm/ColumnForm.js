bd9220452bf6c2e4cd4270032016392d0bee2843
Barbara Kosińska <basia7kosinska@.com>
2024-01-15 16:29:11 +0100

    Add style for input name

--- first-react-pack/src/components/ColumnForm/ColumnForm.js
+++ first-react-pack/src/components/ColumnForm/ColumnForm.js
@@ -1,6 +1,7 @@
 import styles from './ColumnForm.module.scss';
 import { useState } from 'react';
 import Button from '../Button/Button';
+import TextInput from '../TextInput/TextInput';
 
 const ColumnForm = props => {
     const [title, setTitle] = useState('');
@@ -13,9 +14,9 @@ const ColumnForm = props => {
     };
 
 	return (
-        <form onSubmit={handleSubmit}>
-            Title: <input type="text" value={title} onChange={e => setTitle(e.target.value)} />
-            Icon: <input type="text" value={icon} onChange={e => setIcon(e.target.value)} />
+        <form className= {styles.columnForm} onSubmit={handleSubmit}>
+            <span className={styles.input}> Title: </span><TextInput value={title} onChange={e => setTitle(e.target.value)} />
+            <span className={styles.input}> Icon: </span><TextInput value={icon} onChange={e => setIcon(e.target.value)} />
             <Button> ADD COLUMN </Button>
         </form>
 	);
