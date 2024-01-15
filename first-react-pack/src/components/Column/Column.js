63a0ec88ed78af71bc6efa79ade5657837f15e16
Barbara Kosińska <basia7kosinska@.com>
2024-01-15 12:26:29 +0100

    Change style for description in List.js

--- first-react-pack/src/components/Column/Column.js
+++ first-react-pack/src/components/Column/Column.js
@@ -3,7 +3,7 @@ import styles from './Column.module.scss';
 const Column = (props) => {
     return (
         <article className={styles.column}>
-            <h2><span className={styles.icon + ' fa fa-' + props.icon} />{props.title}</h2>       
+            <h2 className={styles.h2}><span className={styles.icon + ' fa fa-' + props.icon} />{props.title}</h2>       
         </article>
     );
 };
--- first-react-pack/src/components/List/List.js
+++ first-react-pack/src/components/List/List.js
@@ -7,8 +7,8 @@ const List = () => {
     <div className= {styles.list}>
         <header className={styles.header}> 
             <h1 className={styles.title}>Things to do<span> soon!</span></h1>
-            <p className={styles.description}>Interesting things I want to check out</p>
         </header>
+        <p className={styles.description}>Interesting things I want to check out</p>
         <section className={styles.columns}>
             <Column title="Books" icon="book"/>
             <Column title="Movies" icon="gamepad"/>
