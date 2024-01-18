c023db3b6453533eb858a2518c4affa5ef87c77b
Barbara Kosińska <basia7kosinska@.com>
2024-01-18 14:06:57 +0100

    Import column and cards data and modify List component

--- first-react-pack/src/components/List/List.js
+++ first-react-pack/src/components/List/List.js
@@ -3,22 +3,27 @@ import styles from './List.module.scss';
 import ColumnForm from '../ColumnForm/ColumnForm';
 import { useSelector } from 'react-redux';
 
-const List = () => {
+const List = (props) => {
 
     const columns = useSelector(state => state.columns);
+    const cards = useSelector(state => state.cards.filter(card => card.columnId === props.id));
 
     return (
-        <div className= {styles.list}>
-            <header className={styles.header}> 
-                <h1 className={styles.title}>Things to do<span> soon!</span></h1>
-            </header>
-            <p className={styles.description}>Interesting things I want to check out</p>
-            <section className={styles.columns}>
-                {columns.map(column => <Column key={column.id} id={column.id} title={column.title} icon={column.icon} cards={column.cards} />)}
-            </section>
-            <ColumnForm/>
+        <div className={styles.list}>
+          <header className={styles.header}>
+            <h2 className={styles.title}>Things to do<span>soon!</span></h2>
+          </header>
+          <p className={styles.description}>Interesting things I want to check out</p>
+          <section className={styles.columns}>
+            {columns.map(column =>
+              <Column
+                key={column.id}
+                {...column}  />
+            )}
+          </section>
+          <ColumnForm />
         </div>
-    )
+      );
 };
 
 export default List;
