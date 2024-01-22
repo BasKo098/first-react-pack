5e96115420d047a73cf1cde94dac5f0139fc81c8
Barbara Kosińska <basia7kosinska@.com>
2024-01-22 13:24:31 +0100

    Render Not Found component in uncorrect app  adress

---
+++ first-react-pack/src/components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import styles from './NotFound.module.scss'
+import PageTitle from '../PageTitle/PageTitle';
+
+const NotFound = (props) => {
+    return(
+        <div className={styles.notFound}>
+            <PageTitle>404 not found</PageTitle>  
+        </div>
+    )
+};
+export default NotFound;
