a681e2edf32b9d346f4eafb5e605df5a59c7e8cb
Barbara Kosińska <basia7kosinska@.com>
2024-01-23 14:53:05 +0100

    Adding a condition to the List component using Navigate to handle invalid list addresses.

--- first-react-pack/src/components/List/List.js
+++ first-react-pack/src/components/List/List.js
@@ -4,19 +4,19 @@ import ColumnForm from '../ColumnForm/ColumnForm';
 import SearchForm from '../SearchForm/SearchForm';
 import { useSelector } from 'react-redux';
 import { getListById, getColumnsByList } from '../../redux/store';
-import { useParams } from 'react-router';
+import { useParams, Navigate } from 'react-router';
 
 const List = (props) => {
     
     const { listId } = useParams();
     const listData = useSelector(state => getListById(state, listId))
-const columns = useSelector(state => getColumnsByList(state, listId));
+    const columns = useSelector(state => getColumnsByList(state, listId));
 
     console.log('List Data:', listData);
     //const listData = useSelector(state => getListById(state, props.listId));
     console.log('columns', columns);
 
-
+    if(!listData) return <Navigate to="/" />
     return (
         <div className={styles.list}>
           <header className={styles.header}>
