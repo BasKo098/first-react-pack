dfef3bb2f5f89863029323e434ca30a3d47867e8
Barbara Kosińska <basia7kosinska@.com>
2024-01-24 09:22:09 +0100

    Fixed problem with add List by spread state

--- first-react-pack/src/redux/store.js
+++ first-react-pack/src/redux/store.js
@@ -25,6 +25,7 @@ export const getFilteredCards = ({ cards, searchString }, columnId) =>
 export const addColumn = payload => ({ type: 'ADD_COLUMN', payload});
 export const addCard = payload => ({ type: 'ADD_CARD', payload});
 export const updateSearchString = payload => ({type: 'UPDATE_SEARCHSTRING', payload});
+export const addList = payload => ({type: 'ADD_LIST', payload});
 
   const reducer = (state, action) => {
     switch (action.type) {
@@ -45,6 +46,12 @@ export const updateSearchString = payload => ({type: 'UPDATE_SEARCHSTRING', payl
                  ...state, 
                  searchString: action.payload 
             };
+
+        case "ADD_LIST":
+            return{
+                ...state,
+                lists: [...state.lists, {...action.payload, id: shortid() }],
+            }
         default:
           return state;
       }
